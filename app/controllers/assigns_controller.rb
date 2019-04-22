fbd603163a6b53db0f82595cddd4e8c7757372aa
sacrament <xkinked.tailx@gmail.com>
2019-04-22 16:31:49 +0900

    teams controllerとassign controllerにprivateメソッドと制限の追加

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -1,5 +1,6 @@
 class AssignsController < ApplicationController
   before_action :authenticate_user!
+  before_action :remove_restrictions, only: [:destroy]
 
   def create
     team = Team.friendly.find(params[:team_id])
@@ -11,7 +12,7 @@ class AssignsController < ApplicationController
       redirect_to team_url(team), notice: 'アサインに失敗しました！'
     end
   end
-
+  # チームからメンバーを削除するアクション
   def destroy
     assign = Assign.find(params[:id])
     destroy_message = assign_destroy(assign, assign.user)
@@ -45,5 +46,26 @@ class AssignsController < ApplicationController
   def set_next_team(assign, assigned_user)
     another_team = Assign.find_by(user_id: assigned_user.id).team
     change_keep_team(assigned_user, another_team) if assigned_user.keep_team_id == assign.team_id
+  end
+
+  # 現在閲覧ページのteamレコードを取得
+  def get_current_team
+    @team = Team.find_by(name: params[:team_id])
+  end
+  # そのチームのオーナーであるならtrueを返す
+  def owner?
+    get_current_team
+    current_user.id == @team.owner_id
+  end
+  # チームから削除するメンバーが自分自身であるならtrueを返す
+  def yourself?
+    @assign = Assign.find(params[:id])
+    current_user.id == @assign.user_id
+  end
+
+  def remove_restrictions
+    unless owner? || yourself?
+      redirect_to team_url(params[:team_id]), notice: "メンバー削除は自身かオーナーのみ実行可能です"
+    end
   end
 end
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,7 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_team, only: %i[show edit update destroy]
+  before_action :update_restrictions, only: [:update]
 
   def index
     @teams = Team.all
@@ -56,4 +57,11 @@ class TeamsController < ApplicationController
   def team_params
     params.fetch(:team, {}).permit %i[name icon icon_cache owner_id keep_team_id]
   end
+
+  def update_restrictions
+    unless @team.owner_id == current_user.id
+      redirect_to @team, notice: "チーム情報編集はteamリーダーのみ実行可能です"
+    end
+  end
+
 end
